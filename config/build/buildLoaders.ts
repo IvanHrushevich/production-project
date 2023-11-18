69d96f2f56357d13e8ab2f5a354560cbab959643
Ivan Grushevich <ivan.grushevich1@gmail.com>
2023-11-19 02:25:59 +0300

    feat: add basic react setup

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,18 @@ const typescriptLoader = {
     exclude: /node_modules/,
 };
 
+const cssLoaders = {
+    test: /\.s[ac]ss$/i,
+    use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+    ],
+};
+
 export function buildLoaders(): webpack.RuleSetRule[] {
-    return [typescriptLoader];
+    return [typescriptLoader, cssLoaders];
 }
--- src/test.ts
+++
@@ -1,4 +0,0 @@
-export function test(num: number) {
-  console.log("ts works");
-  return `${num}`;
-}
